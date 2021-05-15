529d12e1f37e56d6cabf1ada2edb3b4fc6b8513e
tga-business <81005306+tga-business@users.noreply.github.com>
2021-05-15 18:52:58 +0900

    deviseの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates 
+  validates :name, presence: true
+  validates :profile, presence: true
+  validates :occupation, presence: true
+  validates :position, presence: true
+  
 end

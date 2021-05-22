85c55cd5a8867be6b3c78eb934c64e722c4f0671
tga-business <81005306+tga-business@users.noreply.github.com>
2021-05-22 21:54:14 +0900

    protospace実装完了

--- db/migrate/20210515095153_create_prototypes.rb
+++ db/migrate/20210515095153_create_prototypes.rb
@@ -1,7 +1,10 @@
 class CreatePrototypes < ActiveRecord::Migration[6.0]
   def change
-    create_table :prototypes do |t|
-
+    create_table   :prototypes do |t|
+      t.string     :title,      null: false
+      t.text       :cacth_copy, null: false
+      t.text       :concept,    null: false
+      t.references :user,       foregn_key: true
       t.timestamps
     end
   end

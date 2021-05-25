2989edd3cadd987fa6a6f259220fef27c0d30774
tga-business <81005306+tga-business@users.noreply.github.com>
2021-05-25 22:15:36 +0900

    issue

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,7 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, only:[:show, :edit, :updata, :destroy, :move_to_index]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show ]
+  before_action :move_to_index, except: [:index, :show,:new, :create ]
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -11,7 +12,7 @@ class PrototypesController < ApplicationController
   end  
 
   def create
-    @prototype = Prototype.new(prototype_params)
+     @prototype = Prototype.new(prototype_params)
     if @prototype.save
       redirect_to root_path
     else
@@ -20,17 +21,17 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototype = Prototype.find(params[:id])
+    # @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments =@prototype.comments.includes(:user)
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
+    # @prototype = Prototype.find(params[:id])
   end
 
   def update
-    @prototype = Prototype.find(params[:id])
+    # @prototype = Prototype.find(params[:id])
     if @prototype.update(prototype_params)
       redirect_to  prototype_path
     else
@@ -39,7 +40,7 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
-    @prototype = Prototype.find(params[:id])
+    # @prototype = Prototype.find(params[:id])
     @prototype.destroy
     redirect_to root_path
   end
@@ -50,9 +51,13 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :cacth_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
   def move_to_index
-    @prototype= Prototype.find(params[:id])
-    unless user_signed_in? && @prototype.user_id == current_user.id
+
+    unless user_signed_in? @prototype.user_id ==current_user.id
       redirect_to action: :index
     end
   end
